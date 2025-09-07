79688a7ad555e980b1cf74dd5038396c01c6b853
GIODIROSA <giodirosaartes@gmail.com>
2025-09-07 20:26:56 -0300

    [ADD] Estructure list maps iterable

--- 01-dart-intro/02_tipos_datos.dart
+++ 01-dart-intro/02_tipos_datos.dart
@@ -7,8 +7,8 @@ void main() {
   //bool? isAlive;
 
   // Dos formas de tipas el dato
-  final abilities = <String>['impostor'];
-  final List<String> sprites = ['ditto/front.png', 'ditto/background.png'];
+  //final abilities = <String>['impostor'];
+  //final List<String> sprites = ['ditto/front.png', 'ditto/background.png'];
 
   // tiempo de ejecución
   final String variableFinal = "En tiempo de ejecución";
@@ -42,17 +42,42 @@ void main() {
 
   final pokemons2 = {1: 'Abc', 2: 'xyz', 3: 123};
 
-
   print('Name: ${pokemons['name']}');
   print('Name: ${pokemons['sprites']}');
+
+  //final sprites = pokemons['sprites'];
+  //final frontSprite = sprites[1];
+  //final backSprite = sprites[2];
+
+  print('Front Sprites: ${pokemons['sprites'][1]}');
+  print('Back Sprites: ${pokemons['sprites'][2]}');
+
+  // Estructuras de colecciones
+  // Lists, Maps, Iterables, Sets
+
+  final numbers = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 9, 9, 9, 10];
+  print('Listado original: $numbers');
+  print('Length: ${numbers.length}');
+  print('Index 0: ${numbers[0]}');
+  print('First ${numbers.first}');
+  print('Reverse: ${numbers.reversed}');
+
+  final reversedNumbers = numbers.reversed;
+  print('Iterable: $reversedNumbers');
+  print('List: ${reversedNumbers.toList()}');
+  print('Set: ${reversedNumbers.toSet()}');
+
+  final numberGreaterThan5 = numbers.where((num) {
+    return num > 5;
+  });
+
+  print('>5 =>: ${numberGreaterThan5.toSet()}');
 
   // String multi variable
   print("""
   $pokemon
   $hp
   $isAlive
-  $abilities
-  $sprites
   $variableFinal
   $variableNumero
   $constante
@@ -62,9 +87,5 @@ void main() {
   $pokemons
   ******
   $pokemons2
-
   """);
-
-
-
 }
